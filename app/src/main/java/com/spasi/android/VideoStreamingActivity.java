c0de7e60e0fa28803a4a7d57c602ba97e31e92fe
fajarrdp77 <fajarrdp@gmail.com>
2016-01-31 23:13:00 +0700

    Android Unit Test

---
+++ app/src/main/java/com/spasi/android/VideoStreamingActivity.java
@@ -0,0 +1,41 @@
+package com.spasi.android;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class VideoStreamingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_streaming);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        getSupportActionBar().setTitle("Spasi Android | Video Streaming");
+        // Global tv
+        String LINK = "http://livemedia.mncplaymedia.net/cdn/iptv/Tvod/001/channel2000041/index128.m3u8";
+        //setContentView(R.layout.mediaplayer);
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);
+        MediaController mc = new MediaController(this);
+        mc.setAnchorView(videoView);
+        mc.setMediaPlayer(videoView);
+        Uri video = Uri.parse(LINK);
+        videoView.setMediaController(mc);
+        videoView.setVideoURI(video);
+        videoView.start();
+    }
+
+}
